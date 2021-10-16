778b6ef84ea9c6ceb3e1977e0708f2b57833437d
Paludeto <gpalu54@gmail.com>
2021-10-16 01:22:18 -0300

    Aula 5 completa, projetos bytebank-herdado-conta e java-pilha devidamente alterados conforme o aprendido.

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -1,34 +1,26 @@
 public class Fluxo {
-    //testando debugger, entendendo o conceito de call stack, step over e step into.
+
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        try {
+        try{
             metodo1();
-        } catch (ArithmeticException | NullPointerException | MinhaExcecao e) {
-            String mensagem = e.getMessage();
-            System.out.println("Arithmetic Exception " + mensagem);
-            e.printStackTrace();
+        } catch(Exception ex) {
+            String msg = ex.getMessage();
+            System.out.println("Exception " + msg);
+            ex.printStackTrace();
         }
         System.out.println("Fim do main");
     }
 
-    private static void metodo1() throws MinhaExcecao {
+    private static void metodo1()  throws MinhaExcecao{
         System.out.println("Ini do metodo1");
-//        try {
-//            metodo2();
-//        } catch (ArithmeticException e) {
-//            System.out.println("Arithmetic exception");
-//        }
+        metodo2();
         System.out.println("Fim do metodo1");
     }
 
-    private static void metodo2() throws MinhaExcecao {
+    private static void metodo2() throws MinhaExcecao{
         System.out.println("Ini do metodo2");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-            throw new MinhaExcecao("Deu ruim.");
-        }
-        System.out.println("Fim do metodo2");
+        throw new NullPointerException	("deu muito errado");
+        //System.out.println("Fim do metodo2");
     }
-
 }
