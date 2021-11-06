0f32a6ef2ea46a40eb9e476ca8924d092b4d5299
Paludeto <gpalu54@gmail.com>
2021-11-06 11:02:29 -0300

    Prática de referências genéricas e typecasting para acessar objeto no array.

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
@@ -1,12 +1,13 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 
 public class TesteArrayReferencias {
 
     public static void main(String[] args) {
 
-        ContaCorrente[] contas = new ContaCorrente[5];
+        Conta[] contas = new ContaCorrente[5];
 
         ContaCorrente cc1 = new ContaCorrente(22, 33);
         ContaCorrente cc2 = new ContaCorrente(55, 22);
@@ -14,7 +15,10 @@ public class TesteArrayReferencias {
         contas[0] = cc1;
         contas[1] = cc2;
 
-        System.out.println(contas[0].getNumero());
+        System.out.println(contas[1].getNumero());
+        ContaCorrente ref = (ContaCorrente) contas[0];
+        System.out.println(cc2.getNumero());
+        System.out.println(ref.getNumero());
 
     }
 
